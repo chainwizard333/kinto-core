4dd57f4a8edf072bd8ff2268943bd50787ab24e4
chainwizard333 <erikchain3@gmail.com>
2025-04-18 01:53:37 -0400

    Update AbsInbox.sol
    Co-authored-by: legendy4141 <legendy4141@gmail.com>

--- src/nitro-contracts/bridge/AbsInbox.sol
+++ src/nitro-contracts/bridge/AbsInbox.sol
@@ -1,7 +1,3 @@
-// Copyright 2021-2022, Offchain Labs, Inc.
-// For license information, see https://github.com/nitro/blob/master/LICENSE
-// SPDX-License-Identifier: BUSL-1.1
-import {
     DataTooLarge,
     GasLimitTooLarge,
     InsufficientValue,
