f8abbd12478fafa79e9c6219bbd9d463ea1c3d0f
chainwizard333 <erikchain3@gmail.com>
2025-04-18 02:12:42 -0400

    Update AbsInbox.sol
    Co-authored-by: legendy4141 <legendy4141@gmail.com>

--- src/nitro-contracts/bridge/AbsInbox.sol
+++ src/nitro-contracts/bridge/AbsInbox.sol
@@ -1,7 +1,3 @@
-    /// @dev Storage slot with the admin of the contract.
-    /// This is the keccak-256 hash of "eip1967.proxy.admin" subtracted by 1.
-    bytes32 internal constant _ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
-
     /// @inheritdoc IInboxBase
     IBridge public bridge;
     /// @inheritdoc IInboxBase
